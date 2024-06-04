dad15a0ccb4012807e0e0a283bb734052372752a
MilicaDjurkovic <milicadjurkovic90@gmail.com>
2024-06-04 12:20:51 +0200

    fix test_prediction

--- section-05-production-model-package/tests/test_prediction.py
+++ section-05-production-model-package/tests/test_prediction.py
@@ -1,4 +1,5 @@
 import math
+import warnings
 
 import numpy as np
 
@@ -6,17 +7,21 @@ from regression_model.predict import make_prediction
 
 
 def test_make_prediction(sample_input_data):
-    # Given
-    expected_first_prediction_value = 113422
-    expected_no_predictions = 1449
+    # Prigušivanje DeprecationWarning
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=DeprecationWarning)
 
-    # When
-    result = make_prediction(input_data=sample_input_data)
+        # Given
+        expected_first_prediction_value = 113422
+        expected_no_predictions = 1449
 
-    # Then
-    predictions = result.get("predictions")
-    assert isinstance(predictions, list)
-    assert isinstance(predictions[0], np.float64)
-    assert result.get("errors") is None
-    assert len(predictions) == expected_no_predictions
-    assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=100)
+        # When
+        result = make_prediction(input_data=sample_input_data)
+
+        # Then
+        predictions = result.get("predictions")
+        assert isinstance(predictions, list)
+        assert isinstance(predictions[0], np.float64)
+        assert result.get("errors") is None
+        assert len(predictions) == expected_no_predictions
+        assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=100)
