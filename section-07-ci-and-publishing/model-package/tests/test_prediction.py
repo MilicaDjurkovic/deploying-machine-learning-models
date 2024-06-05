60e573298658dfc43a45f30cf932fbccb351e2c6
MilicaDjurkovic <milicadjurkovic90@gmail.com>
2024-06-05 13:37:13 +0200

    fix files

--- section-07-ci-and-publishing/model-package/tests/test_prediction.py
+++ section-07-ci-and-publishing/model-package/tests/test_prediction.py
@@ -1,5 +1,4 @@
 import math
-import warnings
 
 import numpy as np
 
@@ -7,21 +6,17 @@ from regression_model.predict import make_prediction
 
 
 def test_make_prediction(sample_input_data):
+    # Given
+    expected_first_prediction_value = 113422
+    expected_no_predictions = 1449
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=DeprecationWarning)
+    # When
+    result = make_prediction(input_data=sample_input_data)
 
-        # Given
-        expected_first_prediction_value = 113422
-        expected_no_predictions = 1449
-
-        # When
-        result = make_prediction(input_data=sample_input_data)
-
-        # Then
-        predictions = result.get("predictions")
-        assert isinstance(predictions, list)
-        assert isinstance(predictions[0], np.float64)
-        assert result.get("errors") is None
-        assert len(predictions) == expected_no_predictions
-        assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=100)
+    # Then
+    predictions = result.get("predictions")
+    assert isinstance(predictions, list)
+    assert isinstance(predictions[0], np.float64)
+    assert result.get("errors") is None
+    assert len(predictions) == expected_no_predictions
+    assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=100)
