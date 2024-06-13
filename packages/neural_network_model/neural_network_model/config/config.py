73dadb6821ebafcb66379561c80a3845004d83c8
MilicaDjurkovic <milicadjurkovic90@gmail.com>
2024-06-13 13:10:05 +0200

    changes config.py

--- packages/neural_network_model/neural_network_model/config/config.py
+++ packages/neural_network_model/neural_network_model/config/config.py
@@ -3,6 +3,10 @@
 # style
 
 import os
+import logging
+
+_logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 PWD = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_ROOT = os.path.abspath(os.path.join(PWD, '..'))
@@ -18,8 +22,14 @@ ENCODER_NAME = 'encoder'
 
 # MODEL FITTING
 IMAGE_SIZE = 150  # 50 for testing, 150 for final model
-BATCH_SIZE = 10
+BATCH_SIZE = 5
 EPOCHS = int(os.environ.get('EPOCHS', 1))  # 1 for testing, 10 for final model
+
+_logger.info(f"Current working directory: {PWD}")
+_logger.info(f"Package root: {PACKAGE_ROOT}")
+_logger.info(f"Dataset directory: {DATASET_DIR}")
+_logger.info(f"Trained model directory: {TRAINED_MODEL_DIR}")
+_logger.info(f"Data folder: {DATA_FOLDER}")
 
 
 with open(os.path.join(PACKAGE_ROOT, 'VERSION')) as version_file:
@@ -36,3 +46,9 @@ CLASSES_PATH = os.path.join(TRAINED_MODEL_DIR, CLASSES_FILE_NAME)
 
 ENCODER_FILE_NAME = f'{ENCODER_NAME}_{_version}.pkl'
 ENCODER_PATH = os.path.join(TRAINED_MODEL_DIR, ENCODER_FILE_NAME)
+
+
+_logger.info(f"Model path: {MODEL_PATH}")
+_logger.info(f"Pipeline path: {PIPELINE_PATH}")
+_logger.info(f"Classes path: {CLASSES_PATH}")
+_logger.info(f"Encoder path: {ENCODER_PATH}")
